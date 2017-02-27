ade3526dc77628b5a4adcf02dca3fb7888869d2e
Ting Zhou <zhouting@umich.edu>
2017-02-27 17:04:34 -0500

    add youtube video

--- src/youtube.js
+++ src/youtube.js
@@ -12,7 +12,7 @@ function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
         height: '390',
         width: '640',
-        videoId: 'M7lc1UVf-VE',
+        videoId: '8cweW6rQnrw',
         events: {
             'onReady': onPlayerReady,
             'onStateChange': onPlayerStateChange
@@ -22,7 +22,13 @@ function onYouTubeIframeAPIReady() {
 
 // 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
-    event.target.playVideo();
+    // event.target.playVideo();
+    $('#startDemo').click(playVideo);
+    $("#startDemo").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#projects").offset().top
+        }, 2000);
+    });
 }
 
 // 5. The API calls this function when the player's state changes.
@@ -31,10 +37,15 @@ function onPlayerReady(event) {
 var done = false;
 function onPlayerStateChange(event) {
     if (event.data == YT.PlayerState.PLAYING && !done) {
-        setTimeout(stopVideo, 6000);
+        // setTimeout(stopVideo, 6000);
+        // player.getCurrentTime();
         done = true;
     }
 }
 function stopVideo() {
     player.stopVideo();
+}
+
+function playVideo() {
+    player.playVideo();
 }
