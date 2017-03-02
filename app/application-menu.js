316f7eac0d8f463d7bfa420eb51a4cc65cb73a92
Ting Zhou <zhouting@umich.edu>
2017-03-02 16:02:50 -0500

    Modify Menu bar

--- app/application-menu.js
+++ app/application-menu.js
@@ -3,78 +3,95 @@ const { app, BrowserWindow, Menu, shell } = require('electron')
 const mainProcess = require('./tri_demo')
 
 const template = [
-  // Menu template from the last section.
-  {
-    label: 'File',
-    submenu: [
-      {
-        label: 'Open Video',
-        accelerator: 'CommandOrControl+O',
-        click(item, focusedWindow) {
-          mainProcess.openVideoFromUser(focusedWindow)
-        },
-      },
-    ],
-  },
-  {
-    label: 'Window',
-    submenu: [
-      {
-        label: 'Minimize',
-        accelerator: 'CommandOrControl+M',
-        role: 'minimize',
-      },
-      {
-        label: 'Close',
-        accelerator: 'CommandOrControl+W',
-        role: 'close',
-      },
-    ]
-  }
+    // Menu template from the last section.
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Open Video',
+                accelerator: 'CommandOrControl+O',
+                click(item, focusedWindow) {
+                    mainProcess.openVideoFromUser(focusedWindow)
+                },
+            },
+        ],
+    },
+    {
+        label: 'Window',
+        submenu: [
+            {
+                label: 'Minimize',
+                accelerator: 'CommandOrControl+M',
+                role: 'minimize',
+            },
+            {
+                label: 'Close',
+                accelerator: 'CommandOrControl+W',
+                role: 'close',
+            },
+            {
+                label: 'Reload',
+                accelerator: 'CommandOrControl+R',
+                role: 'reload',
+            },
+        ]
+    },
+    {
+        label: 'dev',
+        submenu: [
+            {
+                label: 'Open dev tool',
+                accelerator: 'Option+Command+I',
+                click(item, focusedWindow) {
+                    focusedWindow.webContents.openDevTools()
+                },
+            },
+        ]
+    }
 ]
 
 if (process.platform === 'darwin') {
-  const name = 'TRI demo'
-  template.unshift({
-    label: name,
-    submenu: [
-      {
-        label: `About ${name}`,
-        role: 'about',
-      },
-      { type: 'separator' },
-      {
-        label: 'Services',
-        role: 'services',
-        submenu: [],
-      },
-      { type: 'separator' },
-      {
-        label: 'Hide Others',
-        accelerator: 'Command+Alt+H',
-        role: 'hideothers',
-      },
-      {
-        label: 'Show All',
-        role: 'unhide',
-      },
-      { type: 'separator' },
-      {
-        label: `Quit ${name}`,
-        accelerator: 'Command+Q',
-        click() { app.quit() },
-      },
-    ],
-  })
+    const name = 'TRI demo'
+    template.unshift({
+        label: name,
+        submenu: [
+            {
+                label: `About ${name}`,
+                role: 'about',
+            },
+            { type: 'separator' },
+            {
+                label: 'Services',
+                role: 'services',
+                submenu: [],
+            },
+            { type: 'separator' },
+            {
+                label: 'Hide Others',
+                accelerator: 'Command+Alt+H',
+                role: 'hideothers',
+            },
+            {
+                label: 'Show All',
+                role: 'unhide',
+            },
+            { type: 'separator' },
+            {
+                label: `Quit ${name}`,
+                accelerator: 'Command+Q',
+                click() { app.quit() },
+            },
+        ],
+    })
 
-  const windowMenu = template.find(item => item.label === 'Window')
-  windowMenu.submenu.push(
-    { type: 'separator' },
-    {
-      label: 'Bring All to Front',
-      role: 'front',
-    }
-  )
+    const windowMenu = template.find(item => item.label === 'Window')
+    windowMenu.submenu.push(
+        { type: 'separator' },
+        {
+            label: 'Bring All to Front',
+            role: 'front',
+        }
+    )
 }
 
 module.exports = Menu.buildFromTemplate(template)
--- app/renderer.js
+++ app/renderer.js
@@ -1,6 +1,6 @@
 const {
-  ipcRenderer,
-  remote
+    ipcRenderer,
+    remote
 } = require('electron')
 
 mainProcess = remote.require('./tri_demo')
@@ -19,87 +19,90 @@ let driverMarker
 
 // initialize the Google Map
 const initMap = (lati, longi) => {
-  map = new google.maps.Map(mapDisplay, {
-    zoom: 15,
-    center: new google.maps.LatLng(lati, longi),
-    mapTypeId: 'roadmap'
-  })
+    map = new google.maps.Map(mapDisplay, {
+        zoom: 15,
+        center: new google.maps.LatLng(lati, longi),
+        mapTypeId: 'roadmap'
+    })
 
-  driverMarker = new google.maps.Marker({
-    position: new google.maps.LatLng(lati, longi),
-    title: "driver's current location"
-  })
+    var image = 'img/marker.png';
+    driverMarker = new google.maps.Marker({
+        position: new google.maps.LatLng(lati, longi),
+        title: "driver's current location",
+        icon: image,
+        animation: google.maps.Animation.DROP
+    })
 }
 
 // update the GPS point
 const updateGPSplot = (map, currentLat, currentLng) => {
 
-  driverMarker.setMap(null) 
+    driverMarker.setMap(null) 
 
-  let driverLatLng = new google.maps.LatLng(currentLat, currentLng)
+    let driverLatLng = new google.maps.LatLng(currentLat, currentLng)
 
-  driverMarker = new google.maps.Marker({
-    position: driverLatLng,
-    title: "driver's current location"
-  })
-  driverMarker.setMap(map)
+    driverMarker = new google.maps.Marker({
+        position: driverLatLng,
+        title: "driver's current location"
+    })
+    driverMarker.setMap(map)
 
-  // draw a new line from previous GPS location to the current place
+    // draw a new line from previous GPS location to the current place
 }
 
 // update the link section highlight
 const updateLinkPlot = (map, shape_points) => {
-  let directionsDisplay = new google.maps.DirectionsRender
-  let directionsService = new google.maps.DirectionsService
+    let directionsDisplay = new google.maps.DirectionsRender
+    let directionsService = new google.maps.DirectionsService
 
-  directionDisplay.setMap(map)
+    directionDisplay.setMap(map)
 
-  calculateAndDisplayRoute(directionsService, directionsDisplay, shape_points)
+    calculateAndDisplayRoute(directionsService, directionsDisplay, shape_points)
 }
 
 const calculateAndDisplayRoute = (directionsService, directionsDisplay, shape_points) => {
-  directionServie.route({
-    origin: shape_points[0],
-    destination: shape_points[end],
-    travelMode: 'DRIVING'
-  }, (response, status) => {
-    if (status == 'OK') {
-      directionsDisplay.setDirections(response)
-    } else {
-      window.alert('Directions request failed due to ' + status)
-    }
-  })
+    directionServie.route({
+        origin: shape_points[0],
+        destination: shape_points[end],
+        travelMode: 'DRIVING'
+    }, (response, status) => {
+        if (status == 'OK') {
+            directionsDisplay.setDirections(response)
+        } else {
+            window.alert('Directions request failed due to ' + status)
+        }
+    })
 }
 
 // Listen to the init signal
 ipcRenderer.on('init', (event) => {
 
-  let isl_lati = 42.31995
-  let isl_longi = -83.233371
+    let isl_lati = 42.31995
+    let isl_longi = -83.233371
 
-  initMap(isl_lati, isl_longi)
+    initMap(isl_lati, isl_longi)
 })
 
 // Listen to the update-gps signal
 ipcRenderer.on('update-gps', (event, currentLat, currentLng) => {
-  console.log("event: update-gps")
-  updateGPSplot(map, currentLat, currentLng)
+    console.log("event: update-gps")
+    updateGPSplot(map, currentLat, currentLng)
 })
 
 // Listen to the update-link signal
 ipcRenderer.on('update-link', (event, shape_points) => {
-  console.log("event: update-link")
-  updateLinkPlot(map, shap_points)
+    console.log("event: update-link")
+    updateLinkPlot(map, shap_points)
 })
 
 ipcRenderer.on('opened-video', (event, videoFile) => {
-  console.log("event: opened-video")
-  videoPlayer.src = videoFile 
-  videoPlayer.play()
+    console.log("event: opened-video")
+    videoPlayer.src = videoFile 
+    videoPlayer.play()
 })
 
 videoPlayer.addEventListener('timeupdate', () => {
-  console.log("event: video-player -> ontimeupdate\n" + "currentTime: " + videoPlayer.currentTime)
+    console.log("event: video-player -> ontimeupdate\n" + "currentTime: " + videoPlayer.currentTime)
 
-  mainProcess.getCurrentGPS(videoPlayer.currentTime)
+    mainProcess.getCurrentGPS(videoPlayer.currentTime)
 })
